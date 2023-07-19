f26b34b8512c0462b18fb93508f27628efca2dab
Nemat Mimi <nematmimi01@gmail.com>
2023-07-19 16:17:35 +0300

    move the fuction from product class to inventory to make my code more clear

--- Inventory.cs
+++ Inventory.cs
@@ -1,10 +1,133 @@
 ﻿using System;
 using System.Xml.Linq;
 using Test;
+ static Product ValidProduct(string name)
+{
+    // Find the first product object in the list whose name matches the given name
+    Product product = Product.products.FirstOrDefault(p => p.Name == name);
+
+    return product;
+
+
+}
+
+
+ static void DeleteProduct(string name)
+{
+    // Find the first product object in the list whose name matches the given name
+    Product product = Product.products.FirstOrDefault(p => p.Name == name);
+    if (product != null)
+    {
+        Product.products.Remove(product);
+
+        Console.WriteLine($"'{name}'Product is deleted succesfully.");
+
+    }
+    else
+    {
+
+        Console.WriteLine($"Product with name '{name}' not found.");
+
+    }
+
+}
+
+
+
+ static void UpdateProduct(string name)
+{
+    Product product = Product.products.FirstOrDefault(p => p.Name == name);
+
+    if (product != null)
+    {
+        Console.WriteLine($"[ The product name: {product.Name}, The product price: {product.price}, The product quantity: {product.quantity}]");
+
+        Console.WriteLine("Update the product: ");
+        Console.Write("Product Name: ");
+        string newName = Console.ReadLine();
+        decimal price;
+        Console.Write("Product Price: ");
+
+        bool isValidPrice = decimal.TryParse(Console.ReadLine(), out price);
+        Console.Write("Product Quantity: ");
+        int quantity;
+        bool isValidQuantity = int.TryParse(Console.ReadLine(), out quantity);
+        if (isValidPrice && isValidQuantity)
+        {
+
+            product.Name = newName;
+            product.price = price;
+            product.quantity = quantity;
+        }
+        else
+        {
+            Console.WriteLine("Invalid input for price or quantity. Please enter a valid values");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Product not found.");
+    }
+}
+
+
+static void AddProduct()
+{
+
+    Console.WriteLine("Add a product: ");
+    Console.Write("Product Name: ");
+    string name = Console.ReadLine();
+    Console.Write("Product Price: ");
+    decimal price;
+
+    bool isValidPrice = decimal.TryParse(Console.ReadLine(), out price);
+    Console.Write("Product Quantity: ");
+    int quantity;
+    bool isValidQuantity = int.TryParse(Console.ReadLine(), out quantity);
+    if (isValidPrice && isValidQuantity)
+    {
+
+        Product product = new Product
+        {
+            Name = name,
+            price = price,
+            quantity = quantity
+
+        };
+
+        Product.products.Add(product);
+
+        Console.WriteLine("Product is added.");
+    }
+    else
+    {
+        Console.WriteLine("Invalid input for price or quantity. Please enter a valid values");
+    }
+}
+
+
+ static void DisplayProducts()
+{
+
+    foreach (Product product in Product.products)
+    {
+
+        Console.WriteLine($"[ The product name: {product.Name}, The product price: {product.price}, The product quantity: {product.quantity}]");
+    }
+
+}
+
+
+
+
+
+
+
 bool exit = false;
 
 while (!exit)
 {
+   
     Console.Clear();
     Console.WriteLine("Main Menu");
     Console.WriteLine("1.Add a product");
@@ -19,28 +142,28 @@ while (!exit)
     switch (input)
     {
         case "1":
-            Product.AddProduct();
+            AddProduct();
             Console.ReadLine();
 
             break;
 
         case "2":
             Console.WriteLine("The List of all Products:");
-            Product.DisplayProducts();
+           DisplayProducts();
             Console.ReadLine();
             break;
 
         case "3":
             Console.WriteLine("Please enter product name: ");
             string ProductNameToUpdate = Console.ReadLine();
-            Product.UpdateProduct(ProductNameToUpdate);
+            UpdateProduct(ProductNameToUpdate);
             Console.ReadLine();
             break;
 
         case "5":
             Console.WriteLine("Please enter product name: ");
             string ProductName = Console.ReadLine();
-            Product pro=Product.ValidProduct(ProductName);
+            Product pro=ValidProduct(ProductName);
             if (pro != null)
             {
                 Console.WriteLine("The product information: ");
@@ -61,7 +184,7 @@ while (!exit)
         case "4":
             Console.WriteLine("Please enter the name of product you want to delete: ");
             string DeletedProductName = Console.ReadLine();
-            Product.DeleteProduct(DeletedProductName);
+           DeleteProduct(DeletedProductName);
             Console.ReadLine();
             break;
         case "6":
