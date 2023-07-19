197fbc7e115477acf48c94e8710a6897652458bd
Nemat Mimi <nematmimi01@gmail.com>
2023-07-19 14:40:07 +0300

    make a menu and add a validproduct method in it

--- Inventory.cs
+++ Inventory.cs
@@ -1,7 +1,65 @@
 ﻿using Test;
+bool exit = false;
 
-Product test = new Product();
-decimal? price = Product.ValidProduct("pro1").price;
+while (!exit)
+{
+    Console.Clear();
+    Console.WriteLine("Main Menu");
+    Console.WriteLine("1.Add a product");
+    Console.WriteLine("2.View all products");
+    Console.WriteLine("3.Edit a product");
+    Console.WriteLine("4.Delete a product");
+    Console.WriteLine("5.Search for a product");
+    Console.WriteLine("6. Exit");
+    Console.WriteLine();
+    Console.Write("Enter your choice: ");
+    string input = Console.ReadLine();
+    switch (input)
+    {
+        case "1":
+            Console.WriteLine("You selected Option 1.");
+    
+            Console.ReadLine();
 
-Console.WriteLine(price);
+            break;
+
+        case "2":
+            Console.WriteLine("You selected Option 2.");
+            Console.ReadLine();
+            break;
+
+        case "3":
+            Console.WriteLine("You selected Option 2.");
+            Console.ReadLine();
+            break;
+
+        case "5":
+            Console.WriteLine("Please enter product name: ");
+            string output = Console.ReadLine();
+            Console.WriteLine("The product information: ");
+
+            Console.WriteLine($"The Product Name: {Product.ValidProduct(output).Name}");
+            Console.WriteLine($"The Product Price: {Product.ValidProduct(output).price}");
+            Console.WriteLine($"The Product Quantity: {Product.ValidProduct(output).quantity}");
+
+            Console.ReadLine();
+            break;
+
+        case "4":
+            Console.WriteLine("You selected Option 2.");
+            Console.ReadLine();
+            break;
+        case "6":
+            exit = true;
+            break;
+
+        default:
+            Console.WriteLine("Invalid input. Press enter to try again.");
+            Console.ReadLine();
+            break;
+    }
+}
+
+
+
 
