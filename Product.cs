9b4b87dfa55a4abe3dd33d196d34b3c1263df66b
Nemat Mimi <nematmimi01@gmail.com>
2023-08-09 13:07:17 +0300

    Add ProductRepository

--- Product.cs
+++ Product.cs
@@ -1,24 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Test
+﻿namespace IMS
 {
     public class Product
     {
-        public static List<Product> products = new List<Product>
-        {
-            new Product {Name="pro1",price=22,quantity=44},
-            new Product {Name="pro2",price=24,quantity=44}
-
-        };
-
-        public Product()
-        {
-
-        }
         public string Name { get; set; }
         public decimal? price { get; set; }
         public int quantity { get; set; }
---
+++ ProductRepository.cs
@@ -0,0 +1,89 @@
+﻿namespace IMS
+{
+    public class ProductRepository
+    {
+        public void AddProduct()
+        {
+            Console.WriteLine("Add a product: ");
+            Console.Write("Product Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Product Price: ");
+            decimal price;
+            bool isValidPrice = decimal.TryParse(Console.ReadLine(), out price);
+            Console.Write("Product Quantity: ");
+            int quantity;
+            bool isValidQuantity = int.TryParse(Console.ReadLine(), out quantity);
+            if (isValidPrice && isValidQuantity)
+            {
+                Product product = new Product
+                {
+                    Name = name,
+                    price = price,
+                    quantity = quantity
+                };
+                Inventory.Products.Add(product);
+                Console.WriteLine("Product is added.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input for price or quantity. Please enter a valid values");
+            }
+        }
+        public static Product ValidProduct(string name)
+        {
+            return Inventory.Products.FirstOrDefault(p => p.Name == name);
+        }
+        public  void DeleteProduct(string name)
+        {
+            var Product = ValidProduct(name);
+            if (Product != null)
+            {
+                Inventory.Products.Remove(Product);
+                Console.WriteLine($"'{name}'Product is deleted succesfully.");
+            }
+            else
+            {
+                Console.WriteLine($"Product with name '{name}' not found.");
+            }
+        }
+        public void DisplayProducts()
+        {
+            foreach (Product product in Inventory.Products)
+            {
+                Console.WriteLine($"[ The product name: {product.Name}, The product price: {product.price}, The product quantity: {product.quantity}]");
+            }
+        }
+        public void UpdateProduct(string name)
+        {
+            var product = Inventory.Products.FirstOrDefault(p => p.Name == name);
+            if (product != null)
+            {
+                Console.WriteLine($"[ The product name: {product.Name}, The product price: {product.price}, The product quantity: {product.quantity}]");
+                Console.WriteLine("Update the product: ");
+                Console.Write("Product Name: ");
+                string newName = Console.ReadLine();
+                decimal price;
+                Console.Write("Product Price: ");
+                bool isValidPrice = decimal.TryParse(Console.ReadLine(), out price);
+                Console.Write("Product Quantity: ");
+                int quantity;
+                bool isValidQuantity = int.TryParse(Console.ReadLine(), out quantity);
+                if (isValidPrice && isValidQuantity)
+                {
+                    product.Name = newName;
+                    product.price = price;
+                    product.quantity = quantity;
+                    Console.WriteLine($"'{name}'Product is updated succesfully.");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input for price or quantity. Please enter a valid values");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Product not found.");
+            }
+        }
+    }
+}
